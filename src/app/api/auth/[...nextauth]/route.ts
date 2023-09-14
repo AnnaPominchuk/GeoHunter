0dfd74628182804ece9ca8d3746e3bbb6964238b
Anna Pominchuk <pominchuk.anna@gmail.com>
2023-09-14 12:01:15 +0200

    Auth implementation, appconfig file

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,58 @@
+import NextAuth, {Account, Profile, Session, User} from "next-auth";
+import Auth0Provider from "next-auth/providers/auth0";
+import { JWT } from "next-auth/jwt";
+import { AdapterUser } from "next-auth/adapters";
+
+async function sendTokenToBackend(message: {user: User, account: Account | null, profile? : Profile | undefined, isNewUser? : boolean | undefined }) {
+    const url = `${process.env.NEXT_PUBLIC_MOMENTUM_DEV_URL}/api/v1/user/login`;
+    const headers = new Headers({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${message.account?.access_token}`
+    });
+    const body =  {
+        'email': message.user.email
+    };
+    return await fetch(url, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(body)
+    });
+}
+
+export const authOptions = {
+  providers: [
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID ?? '',
+      clientSecret: process.env.AUTH0_CLIENT_SECRET ?? '',
+      issuer: process.env.AUTH0_ISSUER ?? '',
+      authorization: {
+        params: {
+          audience: encodeURI(process.env.AUTH0_AUDIENCE ?? '')
+        }
+      },
+    })
+  ],
+  events: {
+    signIn:
+        async (message: {user: User, account: Account | null, profile? : Profile | undefined, isNewUser? : boolean | undefined }) => {
+          const result = await sendTokenToBackend(message);
+          if (result.status === 401) {
+            console.error('Authorization failed');
+          }
+    }
+  },
+  callbacks: {
+    async session(params: { session: Session, token: JWT, user: User | AdapterUser}) {
+      return params.session;
+    },
+    async jwt(params: { token: JWT, user?: User | AdapterUser | undefined; account?: Account | null | undefined; profile?: Profile | undefined; isNewUser?: boolean | undefined }) {
+      if (params.account) {
+        params.token.accessToken = params.account.access_token;
+      }
+
+      return params.token;
+    }
+  }
+};
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,20 +1,19 @@
+'use client'
+
 import Link from "next/link";
 import Image from "next/image";
 import '../styles/global.css'
 
-import { useState, useEffect } from "react";
-//import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
-import { getServerSession } from "next-auth";
+export default function Nav(){
 
-const Nav = async () => {
-
-  const session = await getServerSession();
+  const { data: session } = useSession();
 
   return (
     /* Desktop Navigation */
     <div className=''>
-      { !session?.user?.name ? (
+      { !session ? (
         <div className='flex h-screen w-screen gap-3 md:gap-5 items-center justify-center flex-col'>
 
           <input className='purple_shadow rounded-lg p-3 w-1/5'
@@ -31,19 +30,19 @@ const Nav = async () => {
             name="name"
           />
 
-          <button type="button" className='bg-violet-800 px-7 py-2 rounded-lg text-white'>
+          <button type="button" className='bg-violet-800 px-7 py-2 rounded-lg text-white' onClick={() => signIn('auth0')}>
             Sign In
           </button>
 
         </div>
       )
       : (
-       <div>
-        <p>{'You are logged in'}</p>
+       <div className='flex h-screen w-screen gap-3 md:gap-5 items-center justify-center flex-col'>
+        <button type="button" className='bg-violet-800 px-7 py-2 rounded-lg text-white' onClick={() => signOut()}>
+            Log Out
+          </button>
        </div>
       )}
     </div>
   )
 }
-
-export default Nav
